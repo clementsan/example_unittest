ab49952d3b128af111cfad1d89d87ed83c575a71
clementsan <cvachet@sci.utah.edu>
2024-03-15 12:59:11 -0600

    New function: add_one from dependency

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import example.example as e
+import example_package_clem.example as e_clem 
 
 def main():
 	var_add = e.add(2,3)
@@ -9,6 +10,9 @@ def main():
 
 	var_sub = e.substract(3,2)
 	print('var_sub: ', var_sub)
+
+	var_addone = e_clem.add_one(2)
+	print('var_add: ', var_add)
 
 
 if __name__ == "__main__":
